07d49d3dafbc6eaa093ed41b5158d3e0d6670b0d
701031808 <701031808@2260I-L29-018>
2025-02-04 14:01:12 -0500

    Add project files.

---
+++ topic one (printing text)/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace topic_one__printing_text_
+
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Josiah Guevarra
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This will print on a new line");
+
+
+
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+            Console.Write("Hello,world!");
+            Console.Write("This will print on the same line");
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+             
+        }
+    }
+}
